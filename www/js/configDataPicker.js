c639109c6b23b9098635cbd0cea7e08236e527fc
RafaelLimaReis <rafaellimareis01@gmail.com>
2017-03-19 16:02:55 -0300

    commit

---
+++ www/js/configDataPicker.js
@@ -0,0 +1,20 @@
+angular.module('starter').config(function(ionicDatePickerProvider)){
+  var datePickerObj = {
+      inputDate: new Date(),
+      setLabel: 'Set',
+      todayLabel: 'teste',
+      closeLabel: 'Close',
+      mondayFirst: false,
+      weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      templateType: 'popup',
+      from: new Date(2012, 8, 1)
+      to: new Date(2018, 8, 1)
+      showTodayButton: true,
+      dateFormat: 'dd MMMM yyyy',
+      closeOnSelect: false,
+      disableWeekdays: [6],
+    };
+    ionicDatePickerProvider.configDatePicker(datePickerObj);
+  })
+}
